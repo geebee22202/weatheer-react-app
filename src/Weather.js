4fa58487482b8ddcc6f47c2e177cb172dacf8d5a
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-03-29 19:02:13 -0400

    fixed loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Puff } from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleSearch(response) {
@@ -10,13 +11,5 @@ export default function Weather(props) {
   let apiKey = "935a08ec240b922ff7c41b1be851c24f";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
   axios.get(url).then(handleSearch);
-  return (
-    <Loader
-      type="puff"
-      color="#00BFFF"
-      height={100}
-      width={100}
-      timeout={3000}
-    />
-  );
+  return <Puff color="#00BFFF" height={100} width={100} timeout={3000} />;
 }
