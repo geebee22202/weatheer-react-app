a00490b3978cca3a470112f96ec8c2dce831129d
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-03-29 20:28:52 -0400

    added open-source link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <small><a href="https://github.com/geebee22202/weatheer-react-app">Open-source</a> by Georgie</small>
     </div>
   );
 }
