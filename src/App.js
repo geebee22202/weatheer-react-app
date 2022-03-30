6250506abc50f00a70336d18157e355b7b0319b8
geebee22202 <90569659+geebee22202@users.noreply.github.com>
2022-03-29 20:21:53 -0400

    roughly converted previous vanilla javascript weather app to react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello Everyone!</h1>
-        <Weather city="Lisbon"/>
-      </header>
+      <Weather />
     </div>
   );
 }
 
-export default App;
+
+
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,88 @@
 import React from "react";
 import axios from "axios";
-import { Puff } from "react-loader-spinner";
+import "./Weather.css";
+// import { Puff } from "react-loader-spinner";
 
 export default function Weather(props) {
-  function handleSearch(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°F`
-    );
-  }
-  let apiKey = "935a08ec240b922ff7c41b1be851c24f";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
-  axios.get(url).then(handleSearch);
-  return <Puff color="#00BFFF" height={100} width={100} timeout={3000} />;
+  // const [weather, setWeather] = useState("");
+
+  // function displayWeather(response) {
+  //   setWeather({
+  //     temperature: response.data.main.temp,
+  //     wind: response.data.wind.speed,
+  //     humidity: response.data.main.humidity,
+  //     icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+  //     description: response.data.weather[0].description,
+  //   });
+  // }
+
+  // let apiKey = "935a08ec240b922ff7c41b1be851c24f";
+  // let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=imperial`;
+  // axios.get(url).then(displayWeather);
+    let weatherData = {
+      city: "Arlington",
+      date: "Tuesday 9:56p",
+      description: "Cloudy",
+      imgURL: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      temperature: "65",
+      humidity: "45",
+      wind: "12",
+    };
+  return (
+    <div className="Weather">
+      <form className="search-form">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="text"
+              placeholder="Type the name of a city..."
+              autofocus="on"
+              autocomplete="off"
+              className="form-control location-search shadow-sm"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="form-control btn btn-primary shadow-sm"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="overview">
+        <h1>{props.city}</h1>
+        <ul>
+          <li> Last updated: 8:30pm</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temp">
+            <img
+              src={weatherData.imgURL}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                °<a href="/">F</a>{" "}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6 weather-elements">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+{
+  /* <Puff color="#00BFFF" height={100} width={100} timeout={3000} />; */
 }
